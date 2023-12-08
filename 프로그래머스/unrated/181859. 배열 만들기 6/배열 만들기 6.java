774eeb11ab13a345c19bc173a5c85975e670c099
588886 <133185515+588886@users.noreply.github.com>
2023-12-08 14:16:55 +0900

    [level unrated] Title: 배열 만들기 6, Time: 11.61 ms, Memory: 85.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181859. 배열 만들기 6/배열 만들기 6.java
@@ -0,0 +1,23 @@
+
+import java.util.ArrayList;
+class Solution {
+    public ArrayList<Integer> solution(int[] arr) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+		
+		for(int i=0;i<arr.length;i++) {
+			
+			if(list.size()==0) {
+				list.add(arr[i]);
+			}else if(list.get(list.size()-1)==arr[i]) {
+				list.remove(list.size()-1);
+			}else if(list.get(list.size()-1)!=arr[i]) {
+				list.add(arr[i]);
+			}
+			
+		}
+        if(list.size()==0) {
+			list.add(-1);
+		}
+        return list;
+    }
+}
