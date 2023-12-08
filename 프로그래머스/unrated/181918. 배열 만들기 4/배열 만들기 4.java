0a06539039d9d9825551ce044b45e3f74d70e040
588886 <133185515+588886@users.noreply.github.com>
2023-12-08 14:37:27 +0900

    [level unrated] Title: 배열 만들기 4, Time: 5.15 ms, Memory: 78.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181918. 배열 만들기 4/배열 만들기 4.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+class Solution {
+    public int[] solution(int[] arr) {
+        ArrayList<Integer> list= new ArrayList<>();
+        
+        for(int i=0;i<arr.length;i++) {
+			if(list.isEmpty()==true) {
+				list.add(arr[i]);
+			}else if((list.isEmpty()!=true)&&
+                     (list.get(list.size()-1)>=arr[i])) {
+				list.remove(list.size()-1);
+				i--;
+			}else if((list.isEmpty()!=true)&&
+                     (list.get(list.size()-1)<arr[i])) {
+				list.add(arr[i]);
+			}
+			
+		}
+		
+		int[] stk=new int[list.size()];
+		for( int i=0;i<stk.length;i++) {
+			stk[i]=list.get(i);
+		}
+        return stk;
+    }
+}
