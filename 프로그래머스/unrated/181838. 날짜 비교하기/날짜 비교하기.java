931bf01237935e9d87d38924fa32c98fc3b411d6
588886 <133185515+588886@users.noreply.github.com>
2023-12-08 12:53:53 +0900

    [level unrated] Title: 날짜 비교하기, Time: 0.02 ms, Memory: 71.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(int[] date1, int[] date2) {
+        int answer = 
+			((date1[0]*10000)+(date1[1]*100)+(date1[2]))<(date2[0]*10000)+(date2[1]*100)+(date2[2]) ? 1:0;
+		
+        return answer;
+    }
+}
