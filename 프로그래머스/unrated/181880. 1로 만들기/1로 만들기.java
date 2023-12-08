96ab7684b300a3fa9d858cb3b272ecb83b7a899d
588886 <133185515+588886@users.noreply.github.com>
2023-12-08 14:25:49 +0900

    [level unrated] Title: 1로 만들기, Time: 0.02 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181880. 1로 만들기/1로 만들기.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        for(int i=0;i<num_list.length;i++) {
+			
+			for(int i1=0;i1<100;i1++) {
+				
+				if(num_list[i]==1) {
+					break;
+				}else if(num_list[i]%2==1) {
+					num_list[i]=(num_list[i]-1)/2;
+					answer++;
+				}else {
+					num_list[i]/=2;
+					answer++;
+				}
+				
+			}
+			
+		}
+        return answer;
+    }
+}
