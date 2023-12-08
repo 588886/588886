03d35d8905ef577bce4de59c7abb03a3a2e4dcea
588886 <133185515+588886@users.noreply.github.com>
2023-12-08 15:00:22 +0900

    [level unrated] Title: 인덱스 바꾸기, Time: 19.08 ms, Memory: 81.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String my_string, int num1, int num2) {
+        String answer = "";
+        char[] ch=my_string.toCharArray();
+        for(int i=0;i<my_string.length();i++){
+            if(i==num1){
+                 answer+=ch[num2]+"";
+            }else if(i==num2){
+                answer+=ch[num1]+"";
+            }else{
+                answer+=ch[i]+"";
+            }
+        }
+        return answer;
+    }
+}
