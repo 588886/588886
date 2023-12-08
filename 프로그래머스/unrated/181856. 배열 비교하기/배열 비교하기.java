2e3a0a1dcce619ca664d2b481cc466e3a1316c7b
588886 <133185515+588886@users.noreply.github.com>
2023-12-08 14:15:45 +0900

    [level unrated] Title: 배열 비교하기, Time: 0.02 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181856. 배열 비교하기/배열 비교하기.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int solution(int[] arr1, int[] arr2) {
+        int answer=0;
+        if(arr1.length>arr2.length){
+            answer=1;
+        }else if(arr1.length<arr2.length){
+             answer=-1;
+        }else if(arr1.length==arr2.length){
+            int a1=0;
+            int a2=0;
+            
+            for(int i =0;i<arr1.length;i++){
+                a1+=arr1[i];
+            }
+            for(int i =0;i<arr2.length;i++){
+                a2+=arr2[i];
+            }
+            
+            if(a1>a2){
+                answer=1;
+            }else if(a1==a2){
+                answer=0;
+            }else{
+                answer=-1;
+            }
+            
+        }
+        return answer;
+    }
+}
