c9d0dd302e6478f55ab63eb230f6061d8167bb5a
Hyunsic Kim <133185515+588886@users.noreply.github.com>
2024-02-26 16:21:04 +0900

    [level 1] Title: 삼총사, Time: 0.37 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/131705. 삼총사/삼총사.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+class Solution {
+    public int solution(int[] number) {
+        int count=0;
+		Arrays.sort(number);
+		for(int i=0;i<number.length-2;i++) {
+				for(int j=i+1;j<number.length-1;j++) {
+					for(int k=j+1;k<number.length;k++) {
+						if(number[i]+number[j]+number[k]==0)count++;
+					}
+				}
+		}
+        return count;
+    }
+}
